55d657a84c9164205431750f7e20acd3415b5dff
crablet <love4uandgzfc@gmail.com>
2023-03-14 20:14:30 +0800

    feat: 新增submitMessage的代码文件

---
+++ controllers/v1/v1_SubmitMessage.cc
@@ -0,0 +1,5 @@
+#include "v1_SubmitMessage.h"
+
+using namespace v1;
+
+// Add definition of your processing function here
---
+++ controllers/v1/v1_SubmitMessage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <drogon/HttpController.h>
+
+using namespace drogon;
+
+namespace v1
+{
+class SubmitMessage : public drogon::HttpController<SubmitMessage>
+{
+  public:
+    METHOD_LIST_BEGIN
+    // use METHOD_ADD to add your custom processing function here;
+    // METHOD_ADD(SubmitMessage::get, "/{2}/{1}", Get); // path is /v1/SubmitMessage/{arg2}/{arg1}
+    // METHOD_ADD(SubmitMessage::your_method_name, "/{1}/{2}/list", Get); // path is /v1/SubmitMessage/{arg1}/{arg2}/list
+    // ADD_METHOD_TO(SubmitMessage::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
+
+    METHOD_LIST_END
+    // your declaration of processing function maybe like this:
+    // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
+    // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
+};
+}
